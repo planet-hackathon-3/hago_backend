a2393c9dbd6a890ee074ba9a9879843818bdc56a
Leop0ld <l3opold7@gmail.com>
2018-09-01 20:38:26 +0900

    CatImage 캐싱 로직을 전에 있던 리스트는 걸리지 않게끔 변경

--- cats/management/commands/set_random_cat_images.py
+++ cats/management/commands/set_random_cat_images.py
@@ -12,11 +12,18 @@ class Command(BaseCommand):
             raise CommandError("Command doesn't accept any arguments")
 
         try:
+            cached_list = cache.get('random_cat_image_list')
             cat_image_id_list = list(CatImage.objects.values_list('id', flat=True))
+            if cached_list:
+                cached_id_list = [image.id for image in cached_list]
+                for image_id in cat_image_id_list:
+                    if image_id in cached_id_list:
+                        cat_image_id_list.remove(image_id)
+
             random_image_id_list = random.sample(cat_image_id_list, 30)
             cat_images = CatImage.objects.filter(id__in=random_image_id_list)
             cache.set('random_cat_image_list', cat_images, 60 * 30)
         except Exception as e:
-            self.stderr.write(f"Error occured: {str(e)}")
+            self.stderr.write(f"Error occurred: {str(e)}")
         else:
             self.stdout.write(self.style.SUCCESS("Successfully saved random image set"))
