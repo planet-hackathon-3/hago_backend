d2519b4a78c414fc2844b36c307e745923cd8ef9
Leop0ld <l3opold7@gmail.com>
2018-09-01 21:41:09 +0900

    프로젝트 이름 변경 nyan -> hago

--- hago/wsgi.py
+++ hago/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nyan project.
+WSGI config for hago project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nyan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hago.settings')
 
 application = get_wsgi_application()
