e8284c2b201e57047ded2364a959c627edc547f7
Leop0ld <l3opold7@gmail.com>
2018-09-02 00:25:35 +0900

    DATABASES 세팅 변경

--- hago/settings/base.py
+++ hago/settings/base.py
@@ -79,6 +79,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hago.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # NOQA
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
--- hago/settings/local.py
+++ hago/settings/local.py
@@ -1,15 +1,5 @@
 from .base import *  # NOQA
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # NOQA
-    }
-}
-
 # CACHING
 # ------------------------------------------------------------------------------
 CACHES = {
