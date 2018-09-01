74db0f11589596c94636828a37cd137c6118ae57
Leop0ld <l3opold7@gmail.com>
2018-09-02 00:04:26 +0900

    Production Redis 세팅 추가

--- hago/settings/production.py
+++ hago/settings/production.py
@@ -1,3 +1,16 @@
 from .base import *  # NOQA
 
 DEBUG = False
+
+# CACHING
+# ------------------------------------------------------------------------------
+CACHES = {
+    'default': {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://h:p2555d203886181df936adca66d4aa1f5204561e6cd4831200ad8230ef8fdf3aa@"
+                    "ec2-18-209-253-27.compute-1.amazonaws.com:63879",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
