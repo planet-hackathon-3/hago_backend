ff436297aa86fd7d116d5073cf0e2f8387cd0a08
Leop0ld <l3opold7@gmail.com>
2018-09-02 01:10:19 +0900

    set_random_cat_images 커맨드 로직 변경

--- cats/management/commands/set_random_cat_images.py
+++ cats/management/commands/set_random_cat_images.py
@@ -12,14 +12,7 @@ class Command(BaseCommand):
             raise CommandError("Command doesn't accept any arguments")
 
         try:
-            cached_list = cache.get('random_cat_image_list')
             cat_image_id_list = list(CatImage.objects.values_list('id', flat=True))
-            if cached_list:
-                cached_id_list = [image.id for image in cached_list]
-                for image_id in cat_image_id_list:
-                    if image_id in cached_id_list:
-                        cat_image_id_list.remove(image_id)
-
             random_image_id_list = random.sample(cat_image_id_list, 30)
             cat_images = CatImage.objects.filter(id__in=random_image_id_list)
             cache.set('random_cat_image_list', cat_images, 60 * 30)
