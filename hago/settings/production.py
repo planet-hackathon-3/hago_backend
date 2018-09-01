70c7939d0d20bc4ca88c9343e309148fd320bf81
Leop0ld <l3opold7@gmail.com>
2018-09-02 01:38:33 +0900

    STATIC urls 세팅 추가

--- hago/settings/production.py
+++ hago/settings/production.py
@@ -8,7 +8,7 @@ DEBUG = False
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)  # NOQA
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # CACHING
 # ------------------------------------------------------------------------------
