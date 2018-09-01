fa6c70a468d2b87b2a9c9b26df0235f07c8b012f
Leop0ld <l3opold7@gmail.com>
2018-09-02 00:31:58 +0900

    DATABASE 변경 sqlite3 -> PostgreSQL

--- hago/settings/base.py
+++ hago/settings/base.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'hago.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # NOQA
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hago',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
